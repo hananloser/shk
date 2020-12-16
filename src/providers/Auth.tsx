90bf19b31bac6cf8c72ab3ad9d7ba69e712c0919
Hanan Asyrawi <hasyrawi@gmail.com>
2020-12-16 13:04:40 +0800

    prepare for login with high order component

---
+++ src/providers/Auth.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode, ReactElement } from 'react'
+
+
+type AuthContext = {
+    isAuthenticated: boolean,
+    setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const AuthContext = React.createContext<AuthContext>({
+    isAuthenticated: false,
+    setAuthenticated: () => { }
+})
+
+export const AuthProvider = ({ children, authenticated }: { children: ReactNode, authenticated: boolean }): ReactElement => {
+    const [isAuthenticated, setAuthenticated] = React.useState<boolean>(authenticated)
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, setAuthenticated }}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
+
+export function useAuth(): AuthContext {
+    const context = React.useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+}
+
+
+export function useIsAuthenticated(): boolean {
+    const context = useAuth();
+    return context.isAuthenticated;
+}
