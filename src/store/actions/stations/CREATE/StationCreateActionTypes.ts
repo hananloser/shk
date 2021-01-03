6eadafe6398c4948c73ed02d6a9589e06141a8d1
Hanan Asyrawi <hasyrawi@gmail.com>
2021-01-03 12:06:53 +0800

    create action types

---
+++ src/store/actions/stations/CREATE/StationCreateActionTypes.ts
@@ -0,0 +1,34 @@
+/**
+ * STATE FOR CREATE STATION
+ */
+export const STATION_CREATE_LOADING = 'STATION_CREATE_LOADING'
+export const STATION_CREATE_SUCCESS = 'STATION_CREATE_SUCCESS'
+export const STATION_CREATE_FAIL = 'STATION_CREATE_FAIL'
+
+/**
+ * Model Interface
+ * 
+ */
+export interface StationCreate {
+    name_station: string,
+    no_station: string,
+    name: string,
+    username: string,
+    password: string,
+    location: string,
+    photo?: string
+}
+
+export interface StationCreateLoading {
+    type: typeof STATION_CREATE_LOADING
+}
+
+export interface StationCreateSuccess {
+    type: typeof STATION_CREATE_SUCCESS
+    payload: StationCreate
+}
+
+export interface StationCreateFail {
+    type: typeof STATION_CREATE_FAIL
+    message: string | Object | undefined
+}
