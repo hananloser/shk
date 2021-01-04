86dc162b59fc1dfd9240b2db0968b01dc52036d7
Hanan Asyrawi <hasyrawi@gmail.com>
2021-01-04 17:24:45 +0800

    dispatch action create to redux  and store the station

--- src/pages/admin/create.tsx
+++ src/pages/admin/create.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import { Button } from '../../compoents/button'
 import Header from '../../compoents/Header/Index'
 import Inputs from '../../compoents/Inputs/Inputs'
 import router from 'next/router'
 import Modal from '../../compoents/Modal'
+
+import { Button } from '../../compoents/button'
 import { useModal } from '../../providers/ModalProvider'
 import { withAuth } from '../../hoc/withAuth'
 import { AuthToken } from '../../services/auth_token'
-const Create = ({ auth }) => {
-    const { modal, setModal } = useModal()
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { storeStation } from '../../store/actions/stations/CREATE/StationCreateAction'
 
+const Create = ({ auth }) => {
+    const dispatch = useDispatch()
+    const { modal, setModal } = useModal()
+    const { register, handleSubmit } = useForm()
     const user: AuthToken = auth
+
+    const handleForm = (data) => {
+        console.log(data)
+        dispatch(storeStation(data))
+    }
 
     return (
         <div>
@@ -22,84 +33,95 @@ const Create = ({ auth }) => {
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center px-16 mt-10">
-                <div className="card-add w-full bg-alt py-10 rounded-lg space-y-4">
-                    <div className="flex items-center mx-2">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Username</span>
-                            <span className="text-primary">*</span>
+            <form onSubmit={handleSubmit(handleForm)}>
+                <div className="flex justify-center md:px-16 mt-10">
+                    <div className="card-add w-full bg-alt py-10 rounded-lg space-y-4">
+                        <div className="flex items-center mx-2">
+                            <div className="label w-1/6 ml-2 mr-6">
+                                <span className="font-bold">Username</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24 flex-grow-0">
+                                <Inputs name="username" placeholder="Masukan username" ref={register({ required: true })} />
+                            </div>
                         </div>
-                        <div className="flex items-center w-full pr-24 flex-grow-0">
-                            <Inputs name="username" placeholder="Masukan username" />
+                        <div className="flex items-center mx-2">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6">
+                                <span className="font-bold">Name</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24 flex-grow-0">
+                                <Inputs name="name" placeholder="Masukan Nama User" ref={register({ required: true })} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex items-center mx-2 justify-between">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Password</span>
-                            <span className="text-primary">*</span>
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-6">
+                                <span className="font-bold">Password</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="items-center w-full md:pr-24 flex-grow-0">
+                                <Inputs name="password" type="password" placeholder="Masukan password" autoComplete="off" ref={register({ required: true })} />
+                            </div>
                         </div>
-                        <div className="items-center w-full pr-24 flex-grow-0">
-                            <Inputs name="password" placeholder="Masukan password" />
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6">
+                                <span className="font-bold">Nama SPBU</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24">
+                                <Inputs name="name_station" placeholder="Masukan Nama SPBU" ref={register({ required: true })} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex items-center mx-2">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Nama SPBU</span>
-                            <span className="text-primary">*</span>
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6">
+                                <span className="font-bold">Nomor SPBU</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24">
+                                <Inputs name="no_station" placeholder="Masukan No SPBU" ref={register({ required: true })} />
+                            </div>
                         </div>
-                        <div className="flex items-center w-full pr-24">
-                            <Inputs name="name_station" placeholder="Masukan Nama SPBU" />
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6">
+                                <span className="font-bold">Lokasi SPBU</span>
+                                <span className="text-primary">*</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24">
+                                <Inputs name="location" placeholder="Masukan Lokasi SPBU" ref={register({ required: true })} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex items-center mx-2">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Nomor SPBU</span>
-                            <span className="text-primary">*</span>
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6 ">
+                                <span className="font-bold">No. Telpon SPBU</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24">
+                                <Inputs name="phone_number" placeholder="Masukan Phone Number" ref={register({ required: true })} />
+                            </div>
                         </div>
-                        <div className="flex items-center w-full pr-24">
-                            <Inputs name="no_station" placeholder="Masukan No SPBU" />
+                        <div className="flex items-center mx-2 justify-between">
+                            <div className="label w-1/6 ml-2 mr-10 md:mr-6 ">
+                                <span className="font-bold">Link Google Map</span>
+                            </div>
+                            <div className="flex items-center w-full md:pr-24">
+                                <Inputs name="location_link" placeholder="Masukan Lokasi link" ref={register({ required: true })} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex items-center mx-2">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Lokasi SPBU</span>
-                            <span className="text-primary">*</span>
-                        </div>
-                        <div className="flex items-center w-full pr-24">
-                            <Inputs name="location" placeholder="Masukan Lokasi SPBU" />
-                        </div>
-                    </div>
-                    <div className="flex items-center mx-2 flex-shrink-0">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">No. Telpon SPBU</span>
-                        </div>
-                        <div className="flex items-center w-full pr-24">
-                            <Inputs name="phone_number" placeholder="Masukan Phone Number" />
-                        </div>
-                    </div>
-                    <div className="flex items-center mx-2">
-                        <div className="label w-1/6 ml-2">
-                            <span className="font-bold">Link Google Map</span>
-                        </div>
-                        <div className="flex items-center w-full pr-24">
-                            <Inputs name="location_link" placeholder="Masukan Lokasi link" />
-                        </div>
-                    </div>
-                    <div className="flex ml-2 justify-between pt-3 ">
-                        <Button type="button" onClick={() => setModal(!modal)} variant='alternative'>
-                            Tambahkan foto
-                        </Button>
-                        <div className="flex pr-24 space-x-4">
-                            <Button type="button" onClick={() => alert('test')}>
-                                Simpan
+                        <div className="flex ml-2 justify-between pt-3 space-x-1">
+                            <Button type="button" onClick={() => setModal(!modal)} variant='alternative'>
+                                Tambahkan foto
                             </Button>
-                            <Button type="button" onClick={() => router.push('/admin')} variant='outline'>
-                                Batalkan
+                            <div className="flex pr-24 space-x-4">
+                                <Button type="submit">
+                                    Simpan
                             </Button>
+                                <Button type="button" onClick={() => router.push('/admin')} variant='outline'>
+                                    Batalkan
+                            </Button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
             <Modal>
                 <div className="p-2">
                     <div className="flex flex-col item-center justify-center space-y-4 h-44">
--- src/store/actions/stations/CREATE/StationCreateAction.ts
+++ src/store/actions/stations/CREATE/StationCreateAction.ts
@@ -4,12 +4,12 @@ import { Cookies } from 'react-cookie'
 import { StationActionDispatch, STATION_CREATE_FAIL } from './StationCreateActionTypes'
 const cookies = new Cookies()
 
-export const storeStation = () => async (dispatch: Dispatch<StationActionDispatch>) => {
+export const storeStation = (station) => async (dispatch: Dispatch<StationActionDispatch>) => {
     try {
         // Call STATE
         dispatch({ type: 'STATION_CREATE_LOADING' })
         // Fetch To API
-        const res = await API.post(`api/v1/stations`, { headers: { 'Authorization': 'Bearer ' + cookies.get('SHK') } })
+        const res = await API.post(`api/v1/user`, station  ,  { headers: { 'Authorization': 'Bearer ' + cookies.get('SHK') } })
         dispatch({ type: 'STATION_CREATE_SUCCESS', payload: res.data })
     } catch (error) {
         dispatch({ type: STATION_CREATE_FAIL, message: error })
