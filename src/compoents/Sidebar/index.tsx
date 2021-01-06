99ca0812c836867f8c668e24cc4689c7f0972401
Hanan Asyrawi <hasyrawi@gmail.com>
2021-01-07 01:50:15 +0800

    active sidebar menu dashboard

--- src/compoents/Sidebar/index.tsx
+++ src/compoents/Sidebar/index.tsx
@@ -32,11 +32,11 @@ const Sidebar = ({ active = 'deactive' }: Props) => {
     ]
 
     const activeClass = clsx('flex item-center dashboard-logo mx-auto mt-5 text-white  font-roboto font-bold w-full px-4 py-3 rounded-lg cursor-pointer ', Active[active])
-    const dashboard = clsx('flex item-center dashboard-logo mx-auto mt-5 text-white  font-roboto font-bold w-full px-4 py-3 rounded-lg cursor-pointer ')
+    const dashboard = clsx('flex item-center dashboard-logo mx-auto mt-5 text-white  font-roboto font-bold w-full px-4 py-3 rounded-lg cursor-pointer', routePath.pathname === '/dashboard' ? Active[active] : '')
 
     return (
         <div className="hidden md:flex flex-col bg-secondry fixed h-screen w-80 px-2 mt-24 z-50">
-            <div className={dashboard} onClick={() => router.push('/dashboard')}>
+            <div className={dashboard} onClick={() => router.push({ pathname: '/dashboard', query: { station: routePath.query.station as string } })}>
                 <MemoDashboard className="text-2xl mt-1 mx-2" />
                 <span className="tracking text-2xl rounded-2xl">Dashboard</span>
             </div>
@@ -46,7 +46,7 @@ const Sidebar = ({ active = 'deactive' }: Props) => {
                     <ul key={index} className="text-white font-bold mx-auto my-5 ml-5 tracking-wide font-roboto">
                         <Link href={{
                             pathname: item.path,
-                            query: { station: routePath.query.station as string  }
+                            query: { station: routePath.query.station as string }
                         }} passHref >
                             <a className={routePath.pathname === item.path ? activeClass : ''}>{item.name}</a>
                         </Link>
