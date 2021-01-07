5ce9042097d207e358c0fda399e3883718a7b3fc
Hanan Asyrawi <hasyrawi@gmail.com>
2021-01-07 14:42:23 +0800

    fix pop up and add animation

--- src/compoents/CardSpbu/index.tsx
+++ src/compoents/CardSpbu/index.tsx
@@ -4,6 +4,8 @@ import router from 'next/router'
 import MemoDots from '../../assets/icons/Dots'
 import { Station } from '../../store/actions/stations/GET/stationActionTypes'
 import { useModal } from '../../providers/ModalProvider'
+import { Arrow, useLayer } from 'react-laag'
+import { AnimatePresence, motion } from 'framer-motion'
 interface Props {
     name?: string,
     image?: string,
@@ -13,38 +15,73 @@ interface Props {
 
 const CardSpbu = ({ name, image, station, handleId }: Props) => {
 
-    const [popUp, setPopUp] = useState<boolean | undefined>(false)
+    const [isOpen, setOpen] = useState(false)
     const { modal, setModal } = useModal()
+
+    const close = () => {
+        setOpen(false)
+    }
+
+    const { renderLayer, triggerProps, layerProps, arrowProps } = useLayer({
+        isOpen: isOpen,
+        onOutsideClick: close, // close the menu when the user clicks outside
+        onDisappear: close, // close the menu when the menu gets scrolled out of sight
+        overflowContainer: false, // keep the menu positioned inside the container
+        auto: true, // automatically find the best placement
+        placement: "bottom-end", // we prefer to place the menu "top-end"
+        triggerOffset: 12, // keep some distance to the trigger
+        containerOffset: 16, // give the menu some room to breath relative to the container
+        arrowOffset: 16 // let the arrow have some room to breath also
+    });
+
 
     const triggerModal = () => {
         handleId(station.id)
+        setOpen(false)
         setModal(!modal)
     }
 
     return (
-        <div className="ml-2 w-80 bg-gray-200 h-60 rounded-lg relative transform hover:scale-105 duration-200 ease-out" >
-            <img
-                src={image} alt="images"
-                width="240"
-                height="128"
-                className="w-80 h-60 bg-cover rounded-lg"
-                onClick={() => router.push({ pathname: '/dashboard', query: { station: station.id } })}
-            />
-            <div className="flex w-full bg-primary rounded-bl-lg rounded-br-lg absolute bottom-0 p-3 font-bold text-alt">
-                {name}
-            </div>
-            <MemoDots className="text-2xl absolute top-0 right-0 mt-2 cursor-pointer" onClick={() => setPopUp(!popUp)} />
-            {popUp &&
-                <div className="w-32 h-24 text-right bg-alt absolute top-7 right-5 rounded-lg px-3 py-3">
-                    <div className="flex justify-end">
-                        <ul className="font-bold font-sm">
-                            <li className="cursor-pointer" onClick={() => router.push('/admin/edit/' + station.id)}>Ubah Nama </li>
-                            <li className="cursor-pointer" onClick={() => triggerModal()}>Hapus</li>
-                        </ul>
-                    </div>
+        <>
+            <div className="ml-2 w-80 bg-gray-200 h-60 rounded-lg relative transform hover:scale-105 duration-200 ease-out" >
+                <img
+                    src={image} alt="images"
+                    width="240"
+                    height="128"
+                    className="w-80 h-60 bg-cover rounded-lg"
+                    onClick={() => router.push({ pathname: '/dashboard', query: { station: station.id } })}
+                />
+                <div className="flex w-full bg-primary rounded-bl-lg rounded-br-lg absolute bottom-0 p-3 font-bold text-alt">
+                    {name}
                 </div>
-            }
-        </div>
+                <button className="text-2xl absolute top-0 right-0 mt-2 cursor-pointer focus:outline-none" {...triggerProps}>
+                    <MemoDots onClick={() => setOpen(!isOpen)} />
+                </button>
+                {
+                    renderLayer(
+                        <AnimatePresence>
+                            {isOpen && (
+                                <motion.div
+                                    initial={{ opacity: 0, scale: 0.9 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.9 }}
+                                    transition={{ duration: 0.1 }}
+                                    {...layerProps}
+                                    className="w-32 h-24 text-right bg-alt absolute top-7 right-5 rounded-lg px-3 py-3" {...layerProps}>
+                                    <div className="flex justify-end">
+                                        <ul className="font-bold font-sm">
+                                            <li className="cursor-pointer" onClick={() => router.push('/admin/edit/' + station.id)}>Ubah Nama </li>
+                                            <li className="cursor-pointer" onClick={() => triggerModal()}>Hapus</li>
+                                        </ul>
+                                    </div>
+                                    <Arrow {...arrowProps} />
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
+                    )
+                }
+            </div>
+        </>
     )
 }
 
--- src/compoents/Header/Index.tsx
+++ src/compoents/Header/Index.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx'
-import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import React, { useState } from 'react'
+import { Arrow, useLayer } from 'react-laag'
 import MemoAvatar from '../../assets/icons/Avatar'
 import MemoShk from '../../assets/icons/Shk'
 import { usePopup } from '../../providers/PopupProvider'
@@ -29,10 +31,29 @@ type Props = {
 }
 
 const Header = ({ variant = 'manager', title, variantUser, variantTitle, username = '' }: Props) => {
-    const { Popup, setPopup } = usePopup()
+    const [isOpen, setOpen] = useState(false)
     const classHeader = clsx('header px-5 shadow w-full h-24 flex justify-between items-center md:px-16 fixed z-50', Variant[variant])
     const classUsername = clsx('font-bold  mt-1', VariantUser[variantUser])
     const classTitle = clsx('text-3xl font-bold font-roboto', VariantTitle[variantTitle])
+
+
+    const close = () => {
+        setOpen(false)
+    }
+
+    const { renderLayer, triggerProps, layerProps, arrowProps } = useLayer({
+        isOpen: isOpen,
+        onOutsideClick: close, // close the menu when the user clicks outside
+        onDisappear: close, // close the menu when the menu gets scrolled out of sight
+        overflowContainer: false, // keep the menu positioned inside the container
+        auto: true, // automatically find the best placement
+        placement: "bottom-end", // we prefer to place the menu "top-end"
+        triggerOffset: 12, // keep some distance to the trigger
+        containerOffset: 16, // give the menu some room to breath relative to the container
+        arrowOffset: 16 // let the arrow have some room to breath also
+    });
+
+
 
     return (
         <div className={classHeader}>
@@ -44,14 +65,30 @@ const Header = ({ variant = 'manager', title, variantUser, variantTitle, usernam
                 <div className="logo-section">
                 </div>
                 <div className={classUsername}> {username.toUpperCase()} </div>
-                <MemoAvatar className="text-5xl ml-3 cursor-pointer" onClick={() => setPopup(!Popup)} />
-                {Popup && (
-                    <div className="bg-alt absolute top-12 right-12 w-36 h-24 text-right p-4 shadow-lg">
-                        <span className="mb-2 font-semibold cursor-pointer">Lihat Profil</span>
-                        <br />
-                        <span className="mb-2 font-semibold cursor-pointer" onClick={() => AuthToken.logout()}  >Keluar</span>
-                    </div>
-                )}
+                <button  {...triggerProps}>
+                    <MemoAvatar className="text-5xl ml-3 cursor-pointer" onClick={() => setOpen(!isOpen)} />
+                </button>
+                {
+                    renderLayer(
+                        <AnimatePresence>
+                            {isOpen && (
+                                <motion.div
+                                    initial={{ opacity: 0, scale: 0.9 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.9 }}
+                                    transition={{ duration: 0.1 }}
+                                    {...layerProps}>
+                                    <div className="bg-alt z-50 absolute top-0 right-7 w-36 h-24 text-right p-4 shadow-lg rounded-md">
+                                        <span className="mb-2 font-semibold cursor-pointer">Lihat Profil</span>
+                                        <br />
+                                        <span className="mb-2 font-semibold cursor-pointer" onClick={() => AuthToken.logout()}  >Keluar</span>
+                                    </div>
+                                    <Arrow {...arrowProps} />
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
+                    )
+                }
             </div>
         </div>
     )
